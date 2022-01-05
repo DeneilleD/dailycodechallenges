d891177988be9b70142490338495d8d468c63404
Deneille Davids <101914097+DeneilleD@users.noreply.github.com>
2022-01-05 14:00:00 +0100

    i git commit -m 'commit'

---
+++ 6kyu/orderWords/main.js
@@ -0,0 +1,46 @@
+/* 
+INSTRUCTIONS:
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+P: a string 
+R: a new string with the numbers in order
+E: "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+P: turn sting into an array of words with split.
+let stringToArray = words.split(' ')
+sort the words in order by the number in each string.
+*/
+
+// function order(words){
+//     // ...
+//     let newArr = []
+//     let arr = words.split(' ')
+//    for(let i = 0; i < arr.length; i++){
+//     let char = arr[i].split('')
+//     newArr.push(char)
+//     console.log(newArr)
+//    }
+    
+//   }
+
+  //actual solution
+
+  function order(words){
+    var array = words.split(' '); //["is2", "Thi1s", "T4est", "3a"]
+    var sortedArray = [];
+    for(i = 0; i <= array.length; i++) {
+      for(j = 0; j < array.length; j++) {
+        if(array[j].indexOf(i) >= 0) {
+          sortedArray.push(array[j]);
+          console.log(sortedArray)
+        }
+      }
+    }
+    return sortedArray.join(' ');
+  } 
+
+order("is2 Thi1s T4est 3a")  
