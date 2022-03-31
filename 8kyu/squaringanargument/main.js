47fc327c2a95b7fbe65546839c49d216f068a923
Deneille Davids <101914097+DeneilleD@users.noreply.github.com>
2022-03-31 14:00:00 +0100

    i git commit -m "commit"

--- 8kyu/squaringanargument/main.js
+++ 8kyu/squaringanargument/main.js
@@ -8,6 +8,8 @@ P:  create a functin that takes in an integer number and
     returns the square root of number
 
 */  
+
+
 function squareNum(num){ 
     let newNum = num * num;
     return newNum;
