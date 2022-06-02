11d1d70707ded236ab391aab798c8870bc051fbb
Deneille Davids <deneille11@gmail.com>
2022-06-02 15:19:15 -0400

    first update

---
+++ 6kyu/breakCamelCase/main.js
@@ -0,0 +1,18 @@
+/*   Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+ */
+
+function solution(string) {
+  let arr = string.split('')
+  for(let i = 0; i < arr.length; i++){
+      if(arr[i].toUpperCase() === arr[i]){
+          arr.splice(i, 0, ' ')
+          break
+      }
+  }
+  return arr.join('')
+}
