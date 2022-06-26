4e09cfad35638bd9c09ff97879b2ccdc22944c8f
Deneille Davids <101914097+DeneilleD@users.noreply.github.com>
2022-06-26 14:00:00 +0100

    git commit "first commit"

---
+++ 8kyu/squaringanargument/main.js
@@ -0,0 +1,17 @@
+/* Now you have to write a function that takes an argument and returns the square of it.  */ 
+/*  
+P: take in a number 
+R: return number: n^2
+    n*n
+E: n= 3 return=9
+P:  create a functin that takes in an integer number and
+    returns the square root of number
+
+*/  
+
+function squareNum(num){
+    let newNum = num * num;
+    return newNum;
+}
+
+squareNum(2);
