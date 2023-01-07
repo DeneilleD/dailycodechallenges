986e92f31538f903d2685ea50092e5a67f1f5412
Deneille Davids <101914097+DeneilleD@users.noreply.github.com>
2023-01-07 14:25:49 -0500

    Sat Mar 19 14:00 2022 +0100

---
+++ 6kyu/tribonaccisequence/main.js
@@ -0,0 +1,12 @@
+/* you need to create a fibonacci function that given a
+ signature array/list, returns the first n elements - 
+ signature included of the so seeded sequence.  */  
+/*
+P: take in a sequenceof 3 numbers and integer n
+R: return a series of numbers based on the n number(s);
+    always a sequence of 3
+E: [1,2, 3, 6, 5, 7, 18..]
+P: create a function that will take a sequenceof 3 numbers,
+    add and return nth #. so on so forth
+ */
+  
