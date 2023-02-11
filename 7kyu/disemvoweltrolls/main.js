dde9ac5b68a4960b1b133f348ec612cab8c7b3b7
Deneille Davids <101914097+DeneilleD@users.noreply.github.com>
2023-02-11 14:00:00 +0100

    git commit 'commit'

--- 7kyu/disemvoweltrolls/main.js
+++ 7kyu/disemvoweltrolls/main.js
@@ -12,8 +12,6 @@ Note: for this kata y isn't considered a vowel.
    */    
 
 
-
-
 function disemvowel(str) {
   const vowels = ['a','e','i','o','u','A','E','I','O','U']
   return str.split('').filter(el => !vowels.includes(el)).join('')
