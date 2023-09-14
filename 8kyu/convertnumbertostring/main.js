0beb11493ab23de0ced7edeb36b43240184db5ed
Deneille Davids <101914097+DeneilleD@users.noreply.github.com>
2023-09-14 14:00:00 +0100

    git commit -m 'fixed a bug'

--- 8kyu/convertnumbertostring/main.js
+++ 8kyu/convertnumbertostring/main.js
@@ -1,7 +1,9 @@
 /*  We need a function that can transform a number into a string.
 
 What ways of achieving this do you know? */
-function numberToString(num) {
+
+
+  function numberToString(num) {
   // Return a string of the number here!
 
   return num.toString()
